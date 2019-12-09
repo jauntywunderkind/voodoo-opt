03301241477f1fa13d1c94408ec440142a16c3e8
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-12-08 22:53:56 -0500

    bring introspect towards new config

--- introspect.js
+++ introspect.js
@@ -1,24 +1,17 @@
 "use module"
+import Call from "./call.js"
 import { bus, get} from "./config.js"
 import Interface from "./interface.js"
-import Call from "./call.js"
 
-export async function introspect( opts){
-	let
-	  bus= get( "bus", opts),
-	  busName= get( "busName", opts),
-	  objectPath= get( "objectPath", opts),
-	  
-
-	  bus= 
-	  bus= opts.bus&& opts.bus()|| bus(),
-	  busName= opts.busName&& opts.busName()|| opts.busName,
-	  objectPath= opts.path&& opts.path()
-	  iface= Interface(
-		bus,
-		busName,
-		
-		"org.freedesktop.DBus.Introspectable.Introspect")
-	return Call( iface, "Introspect")
+export async function introspect( ...opts){
+	const iface= await Interface({
+		busName: null,
+		objectPath: null,
+		interfaceName: "org.freedesktop.DBus.Introspectable"
+	}, ...opts)
+	return Call({
+		iface,
+		method: "Introspect"
+	}, ...opts)
 }
 export default introspect
