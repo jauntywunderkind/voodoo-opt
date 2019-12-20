06e401d2929f92d4d3a8396edd2f2708929054d3
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-12-19 21:20:50 -0500

    add missing import in introspect.

--- introspect.js
+++ introspect.js
@@ -1,7 +1,12 @@
 "use module"
 import Call from "./call.js"
 import Interface from "./interface.js"
+import { get} from "./opts.js"
 
+/**
+* @param busName - the dbus service
+* @param objectPath - the interface being looked for
+*/
 export async function introspect( ...opts){
 	const iface= await Interface({
 		busName: null,
